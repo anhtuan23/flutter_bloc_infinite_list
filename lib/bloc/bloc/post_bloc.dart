fc5ca7abe39d1d479560a0b5727b5278dbed4d46
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-06-30 10:26:38 +0700

    configure PostBloc

--- lib/bloc/bloc/post_bloc.dart
+++ lib/bloc/bloc/post_bloc.dart
@@ -1,20 +1,77 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:bloc/bloc.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc_infinite_list/models/models.dart';
+import 'package:http/http.dart' as http;
+import 'package:meta/meta.dart';
 
 part 'post_event.dart';
 part 'post_state.dart';
 
 class PostBloc extends Bloc<PostEvent, PostState> {
+  final http.Client httpClient;
+
+  PostBloc({@required this.httpClient});
+
   @override
   PostState get initialState => PostInitial();
-  
+
+  @override
+  Stream<Transition<PostEvent, PostState>> transformEvents(
+      Stream<PostEvent> events, transitionFn) {
+    return super.transformEvents(
+        events.debounceTime(const Duration(milliseconds: 500)), transitionFn);
+  }
+
   @override
   Stream<PostState> mapEventToState(
     PostEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    final currentState = state;
+
+    if (event is PostFetched && !_hasReachedMax(currentState)) {
+      try {
+        if (currentState is PostInitial) {
+          final posts = await _fetchPosts(0, 20);
+          yield PostSuccess(posts: posts, hasReachedMax: false);
+          return;
+        }
+        if (currentState is PostSuccess) {
+          final posts = await _fetchPosts(currentState.posts.length, 20);
+          yield posts.isEmpty
+              ? currentState.copyWith(hasReachedMax: true)
+              : PostSuccess(
+                  posts: currentState.posts + posts,
+                  hasReachedMax: false,
+                );
+        }
+      } catch (_) {
+        yield PostFailure();
+      }
+    }
+  }
+
+  bool _hasReachedMax(PostState state) =>
+      state is PostSuccess && state.hasReachedMax;
+
+  Future<List<Post>> _fetchPosts(int startIndex, int limit) async {
+    final response = await httpClient.get(
+      'https://jsonplaceholder.typicode.com/posts?_start=$startIndex&_limit=$limit',
+    );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body) as List;
+      return data.map((rawPost) {
+        Post(
+          id: rawPost['id'],
+          title: rawPost['title'],
+          body: rawPost['body'],
+        );
+      }).toList();
+    } else {
+      throw Exception('Error fetching posts');
+    }
   }
 }
